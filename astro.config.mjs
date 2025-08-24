69fc29b8e775171dec652e2f542e719e9fdd9c29
Stephen Collins <sgcollins@outlook.com>
2025-08-24 17:51:08 -0400

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://elsebeneath.online",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
