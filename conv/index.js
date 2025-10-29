0635d6cdf28ddc8f7539cf73470593ea6965a037
Stephen Collins <sgcollins@outlook.com>
2025-10-28 20:00:32 -0400

    removed old stuff

--- conv/index.js
+++
@@ -1,8 +0,0 @@
-import { convertMarkdownToHTML, rewritePronouns } from "./lib.js";
-import fs from "fs";
-
-const original = fs.readFileSync("book.md", "utf8");
-// const personalized = rewritePronouns(original, "Sam", "Sam", "he");
-const result = convertMarkdownToHTML(original);
-
-fs.writeFileSync("book.html", result);
