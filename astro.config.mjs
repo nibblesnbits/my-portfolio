3f5cffd3604198053d63ee2cb7e108a522047ed9
Stephen Collins <sgcollins@outlook.com>
2026-03-04 07:55:03 -0500

    added redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,13 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  redirects: {
+    "/story/village": "https://elsebeneath.online/story/village",
+    "/story/valley": "https://elsebeneath.online/story/valley",
+    "/story/elsewhere": "https://elsebeneath.online/story/elsewhere",
+    "/story/hair": "https://elsebeneath.online/story/hair",
+    "/story/brynn": "https://elsebeneath.online/story/brynn",
+    "/story/mountain": "https://elsebeneath.online/story/mountain",
+  },
   integrations: [react(), sitemap()],
 });
